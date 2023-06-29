75178a3172873c594158498a0867d15161621849
Guilherme Potter Petry <guipotterrp@gmail.com>
2023-06-29 16:41:32 -0300

    aula Manipulando arquivos Json com Dart  concluída

--- lib/dart_consumo_api.dart
+++ lib/dart_consumo_api.dart
@@ -1,3 +1,25 @@
+import 'dart:convert';
+
 void main() {
-  
+  final json = '''
+  {
+    "cep": "01001-000",
+    "logradouro": "Praça da Sé",
+    "complemento": "lado ímpar",
+    "bairro": "Sé",
+    "localidade": "São Paulo",
+    "uf": "SP",
+    "ibge": "3550308",
+    "gia": "1004",
+    "ddd": "11",
+    "siafi": "7107"
+  }
+  ''';
+  final jsonData = jsonDecode(json);
+  //final jsonData = jsonDecode(json);
+  print(jsonData.runtimeType);
+  print(jsonData['logradouro']);
+
+  final jsonMap = {'curso': 'Academia do Flutter', 'totalAlunos': 1000};
+  print(jsonEncode(jsonMap));
 }
